7e87efc71b26992b23eb4825f68c2afe258344f7
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-22 14:40:08 +0000

    Added start game button

--- client/src/Pages/Homepage/index.jsx
+++ client/src/Pages/Homepage/index.jsx
@@ -1,13 +1,21 @@
 import React from "react"
+import { useNavigate } from 'react-router-dom'
 
 const logo = require("../../assets/ChessRacerZ_logo.png")
 
 function Homepage() {
  
-  return<>
+    const navigate = useNavigate()
+
+    function sendToGame() {
+        navigate('/game')
+    }
+
+    return<>
             <main>
                 <img src={logo} alt="Logo" />
                 <h1>ChessRacerZ</h1>
+                <button onClick={sendToGame}>Start Game</button>
             </main>
         </>
 }
