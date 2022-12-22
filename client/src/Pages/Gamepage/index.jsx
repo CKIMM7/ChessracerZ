1b79241d2657968e8fa5acace51c005651592e93
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-22 14:33:11 +0000

    Added gampage style sheet

--- client/src/Pages/Gamepage/index.jsx
+++ client/src/Pages/Gamepage/index.jsx
@@ -2,12 +2,16 @@ import React from "react"
 
 import { Header } from "../../components"
 
+import "./gamepage.css"
+
 function Gamepage() {
  
   return<>
             <Header />
             <main>
-                
+                <div id="chess-game">
+                    <p>Chess Game</p>
+                </div>
             </main>
         </>
 }
