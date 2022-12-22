cec9dbad04801893a3c4bd6d4642c9be66e4e0e0
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-22 14:02:38 +0000

    Working on header and styling

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import React from "react"
 
+
 import { GoogleLogin } from "./components"
+import "./header.css"
+
+
+const logo = require("../../assets/ChessRacerZ_logo.png")
 
 function Header() {
  
   return <header>
-           <GoogleLogin />
+            <img src={logo} alt="Logo" />
+            <h1>ChessRacerZ</h1>
+            <GoogleLogin />
         </header>
 
 }
