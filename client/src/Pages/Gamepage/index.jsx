fa5abab746f0b5a4350445748641f153f50bd653
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-23 12:15:32 +0000

    Added waiting for opponent box which turns into countdown when they join - only works for first player currently

--- client/src/Pages/Gamepage/index.jsx
+++ client/src/Pages/Gamepage/index.jsx
@@ -1,18 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import { useLocation } from 'react-router-dom'
 import { socket } from "../../socket"
 
 import { Header, Board } from "../../components"
 
 import "./gamepage.css"
+import { useEffect } from "react"
 
 function Gamepage() {
  
+    const [waitMessage, setWaitMessage] = useState("Waiting on opponent...")
+
     const { state } = useLocation()
     const { lobbyId } = state
 
     console.log(lobbyId)
 
+    useEffect
     socket.on("console-message", function(msg){
         console.log(msg)
         if (msg.includes(`joined ${lobbyId} succesfully`)){
@@ -25,10 +29,25 @@ function Gamepage() {
         console.log("end")
     })
 
+    socket.on("start-game", function() {
+        let countdown = 4
+
+        const countdownInterval = setInterval(function() {
+            countdown--;
+            setWaitMessage(`Game starting in ${countdown} seconds...`)
+            if (countdown === 0) {
+              clearInterval(countdownInterval)
+              document.getElementById("waiting").style.display = "none"
+            }
+        }, 1000);
+
+      });
+
     return<>
                 <Header />
                 <main>
                     <p>Lobby: {lobbyId}</p>
+                    <div id="waiting">{waitMessage}</div>
                     <div id="chess-game">
                         <p>Chess Game</p>
                         <Board/>
