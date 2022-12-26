e5b39072aaeb0e4e8f26edcb1731d353fdb441d0
YKIM <dyounggkim@gmail.com>
2022-12-26 12:17:48 +0000

    save to racing-staging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import { Gamepage, Homepage } from "./Pages"
+import Game from "./components/Race"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/game" element={<Gamepage />}></Route>
+          <Route path="/game2" element={<Game />}></Route>
         </Routes>
 }
 
