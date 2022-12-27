a675cc02b5550f884e404dcff0d28fc335b3d38c
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-27 20:24:55 +0000

    Added functioning timer component

---
+++ client/src/components/Timer/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useRef } from "react"
+import { socket } from '../../socket'
+
+function Timer() {
+
+    const [timer, setTimer] = useState(20)
+    const timerRef = useRef(timer) 
+
+    useEffect(() => {
+        socket.on("start-timer", () => {
+            console.log("starting timer")
+            setTimer(20)
+            timerRef.current = timer
+            const timerInterval = setInterval(() => {
+                timerRef.current -= 1
+                setTimer(timerRef.current)
+                if (timerRef.current === 0){
+                    clearInterval(timerInterval)
+                }
+            }, 1000)
+        })
+
+    }, [])
+
+    return <div id="timer">
+            <p>{Math.floor(timer / 60)} : {(timer % 60)}</p>
+        </div>
+    
+}
+
+export default Timer;
