360a9e3c3216348d84f38c7e635f431317cb2a67
YKIM <dyounggkim@gmail.com>
2022-12-30 16:57:05 +0000

    unimport eventlet

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-import eventlet
-from eventlet import wsgi
-
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow
 
