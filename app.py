63f1f50c29869aa27429cdbd1d9e301d2cbb8bd9
Findlay20 <115260619+Findlay20@users.noreply.github.com>
2023-01-04 18:00:42 +0000

    Updated time to 60s per round

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def join_lobby(lobbyId):
 def startTimer(lobbyId):
   socketio.sleep(3)
   print(f"Lobby {lobbyId}: Starting timer")
-  time = 8
+  time = 90
   socketio.emit("start-timer", time ,room=lobbyId)
   socketio.sleep(time)
   print(f"Lobby {lobbyId}: Timer finished")
