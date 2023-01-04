b281d292493d10145536a8f56f077b1027e24d97
Findlay20 <cameron.findlay@hotmail.co.uk>
2023-01-04 10:52:08 +0000

    Socket testing started

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,24 +1,28 @@
 import pytest
-from app import socketio, create_lobby
+from unittest.mock import MagicMock
+from app import app, socketio
 
-# manager = SocketIOManager()
+socketio.emit = MagicMock()
 
-# socketio = SocketIO()
+flask_test_client = app.test_client()
 
-# request = type('Request', (object,), {'sid': 'dummy-sid'})
+socketio_test_client = socketio.test_client(app, flask_test_client=flask_test_client)
 
-# socketio.server = type('Server', (object,), {'manager': manager})
+def test_can_connect():
+    assert socketio_test_client.is_connected()
+
 
 def test_create_lobby():
-    create_lobby("TestLobby")
+
+    socketio_test_client.emit("create-lobby", "TestLobby")
 
     assert "TestLobby" in socketio.server.manager.rooms['/']
-    assert socketio.emit.call_args_list[-1][0][0] == 'console-message'
-    assert socketio.emit.call_args_list[-1][0][1] == f"{request.sid} created {lobby_id} succesfully"
-    assert socketio.emit.call_args_list[-2][0][0] == 'send-to-game'
-    assert socketio.emit.call_args_list[-2][0][1] == 'w'
-    assert socketio.emit.call_args_list[-2][1]['room'] == request.sid
+    assert socketio.emit.call_args_list[0][0][0] == 'console-message'
+    assert "created TestLobby succesfully" in socketio.emit.call_args_list[0][0][1]
+    assert socketio.emit.call_args_list[-1][0][0] == 'send-to-game'
+    assert socketio.emit.call_args_list[-1][0][1] == 'w'
 
-    create_lobby("TestLobby")
+    socketio_test_client.emit("create-lobby", "TestLobby")
     assert socketio.emit.call_args_list[-1][0][0] == 'console-message'
     assert socketio.emit.call_args_list[-1][0][1] == "Lobby name already taken"
+
