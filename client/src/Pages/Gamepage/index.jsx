f993dddb7532ae5c1919daa2cf64fb6b18d25fb1
Findlay20 <cameron.findlay@hotmail.co.uk>
2023-01-05 12:00:24 +0000

    Displays lobby id in waiting for player box

--- client/src/Pages/Gamepage/index.jsx
+++ client/src/Pages/Gamepage/index.jsx
@@ -11,7 +11,7 @@ import "./gamepage.css"
 
 function Gamepage() {
  
-    const [waitMessage, setWaitMessage] = useState("Waiting on opponent...")
+
     const [draggable, setDraggable] = useState(false)
     const [round, setRound] = useState(1)
 
@@ -19,6 +19,7 @@ function Gamepage() {
     const { state } = useLocation()
     const { lobbyId, color } = state
 
+    const [waitMessage, setWaitMessage] = useState(`Waiting on opponent... \n  Room code: ${lobbyId}`)
 
     useEffect(() => {
         socket.on("console-message", function(msg){
